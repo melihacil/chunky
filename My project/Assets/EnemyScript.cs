5762b43d0af3b55bc824d648d361565d0f95a1d7
melihacil <knghtmellomello@gmail.com>
2022-09-16 20:46:52 +0300

    Adding EnemyScript

---
+++ My project/Assets/EnemyScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private float sightRange;
+    [SerializeField] private float moveSpeed;
+
+
+    private Rigidbody2D rb;
+    Vector2 distance;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = transform.position - playerTransform.position;
+        if (distance.magnitude <= sightRange)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, playerTransform.position, moveSpeed);
+            rb.MovePosition(playerTransform.position);
+        }
+    }
+}
