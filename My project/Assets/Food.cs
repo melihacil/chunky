9cc52ab98b017324b6ead64344d56a27b3ebe083
melihacil <knghtmellomello@gmail.com>
2022-09-17 16:50:30 +0300

    Adding food assets

---
+++ My project/Assets/Food.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+
+
+    [SerializeField] private float healthVal;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.layer == 9)
+        {
+
+        }
+    }
+}
