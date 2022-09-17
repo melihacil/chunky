eafcb4b61d8dc06f51c00e8be9633973cc287593
melihacil <knghtmellomello@gmail.com>
2022-09-17 12:51:53 +0300

    Update PlayerStats.cs

--- My project/Assets/PlayerStats.cs
+++ My project/Assets/PlayerStats.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerStats : MonoBehaviour
 {
+    [SerializeField] private float health;
+    [SerializeField] private float damageVal;
+
+
+    [SerializeField] private Slider healthBar;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        healthBar.maxValue = health;
+        healthBar.value = health;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         health -= damageVal * Time.deltaTime;
+        Debug.Log(health);
+        healthBar.value = health;
     }
 }
